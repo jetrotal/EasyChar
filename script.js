484575f952ef437cdbbb0c7322d98f805d6faa5d
Mauro Junior <45118493+jetrotal@users.noreply.github.com>
2022-12-27 05:09:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -186,7 +186,7 @@ for (var n in charData.layers) {
    
   charData.layers[n].bmp2.y = charData.layers[n].bmp.y = 
     (charData.size.current=="adult") ?
-    (charData.layers[n].img.getAttribute('class') =="clothes" && 
+    (( charData.layers[n].img.getAttribute('class') =="clothes" || charData.layers[n].img.getAttribute('class') =="clothesB" ) && 
 getQuery('body-type',"#qtd").value <= 2) ? 
     charData.layers[n].bmp.y = charData.yOffset.adultNew :  charData.yOffset.adult : 
   charData.layers[n].childOffset; 
